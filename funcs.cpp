932b9605ec15483bd44b319bdb7da6239c3c424c
Ashfak Uddin <ashmanpapet@gmail.com>
2021-09-06 09:39:32 -0400

    Added funcs.cpp for Task B

---
+++ funcs.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Ashfak Uddin
+Course: CSCI-136
+Instructor: Mike Zamansky
+Assignment: Lab2B
+
+This file contains the print_interval function, which prints out every number
+betweeen L and U, including L, but excluding U. It will be used in main.cpp.
+*/
+#include <iostream>
+
+void print_interval(int L, int U)
+{
+	for (int i = L; i < U; i++)
+		std::cout << i << " ";
+}
