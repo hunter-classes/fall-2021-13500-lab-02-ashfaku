7a05c6dbdf87c2cabae933cc3fcd93fea526ccc5
Ashfak Uddin <ashmanpapet@gmail.com>
2021-09-06 09:41:28 -0400

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Ashfak Uddin
+Course: CSCI-136
+Instructor: Mike Zamansky
+Assignment: Lab2B
+
+This file's main method usses funcs.cpp's print_interval method.
+*/
+
+#include <iostream>
+#include "funcs.cpp";
+int main()
+{
+	print_interval(1, 10);
+	return 0;
+}
