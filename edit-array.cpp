4a9a8fb1b7a0ced5f747be309ab640b223d9c3b0
Ubuntu <ashfaku@JavaMySQLProgram.fenqkdcuahnejkn4pji3daeorf.bx.internal.cloudapp.net>
2021-09-12 19:01:53 +0000

    Task C

---
+++ edit-array.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+void print(int a[], int size)
+{
+	for (int i = 0; i < size; i++)
+  		std::cout << a[i] << (i != size - 1? ", " : "\n");
+}
+bool isValidIndex(int a, int lowerBound, int upperBound)
+{
+	return a >= lowerBound && a < upperBound;
+}
+int main()
+{
+	int data[10];
+	for (int i = 0; i < 10; i++)
+	     data[i] = 1;
+	print(data, 10);
+	int index, value;
+	std::cout << "Input index: ";
+	std::cin >> index;
+	std::cout << "Input value: ";
+	std::cin >> value;
+	while (isValidIndex(index, 0, 10))
+	{
+		data[index] = value;
+		print(data, 10);
+		std::cout << "Input index: ";
+		std::cin >> index;
+		std::cout << "Input value: ";
+		std::cin >> value;
+	}
+	std::cout << "Index out of range. Exit." << std::endl;
+	return 0;
+}
+
